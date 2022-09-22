d52e6829f310861f45897bbc582191d94fa8c5a6
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-09-22 17:54:37 +0200

    add cart

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@
         //WELCOME
         Console.WriteLine("Hi welcome to myStore, first of all you have to create a invetory. Then you have to add" +
                            "some product to the shopping cart. After you may wont to precede to checkout ");
+        //STORE
+        Store storage = new Store();
 
         //ACTION
         int action = chooseAction();
@@ -13,6 +15,45 @@
         {
             Console.WriteLine("you chose" + action);
             action = chooseAction();
+
+            switch (action)
+            {
+                //ADD TO THE INVENTORY
+                case 1:
+                    Console.WriteLine("you chose to add product to the inventory");
+                    string shirtMake = "";
+                    string shirttModel = "";
+                    int shirttPrice = 0;
+
+                    Console.WriteLine("what is the product make?"); 
+                    shirtMake = Console.ReadLine();
+
+                    Console.WriteLine("what is the product model?");
+                    shirttModel = Console.ReadLine();
+
+                    Console.WriteLine("what is the product price?");
+                    shirttPrice = int.Parse(Console.ReadLine());
+
+                    Shirt newShirt = new Shirt(shirtMake, shirttModel, shirttPrice);
+                    storage.ShirtList.Add(newShirt);
+
+                    printStorage(storage);
+                    break;
+
+                //ADD TO CART
+                case 2:
+                    Console.WriteLine("you chose to add product to the cart");
+                    printStorage(storage);
+
+                    Console.WriteLine("what product do you wont to buy");
+                    int shirtChosen = int.Parse(Console.ReadLine());
+                    storage.Shoppinglist.Add(storage.ShirtList[shirtChosen]);
+
+                    printShoppingCart();
+
+                    break;
+            }
+
         }
          
 
@@ -32,6 +73,24 @@
         //Console.WriteLine("store value is " + total);
          
     }
+
+    private static void printShoppingCart(Store storage)
+    {
+        Console.WriteLine("Product that you have chosen");
+        for (int i = 0; i < storage.Shoppinglist.Count; i++)
+        {
+            Console.WriteLine("shirt # " + i + " " + storage.Shoppinglist[i]);
+        }
+    }
+
+    private static void printStorage(Store storage)
+    {
+        for( int i = 0; i < storage.ShirtList.Count; i++)
+        {
+            Console.WriteLine("shirt # " + i + " " + storage.ShirtList[i]);
+        }
+    }
+
     static public int chooseAction()
     {
         int choice = 0;
--- Shirt.cs
+++ Shirt.cs
@@ -4,12 +4,22 @@ public class Shirt
     public string Model { get; set; }
     public decimal Price { get; set; }
 
-    
+    public Shirt()
+    {
+        Make = "";
+        Model = "";
+        Price = 0;
+
+    }
 
     public Shirt(string make, string model, decimal price)
     {
         Make = make;
         Model = model;
         Price = price;
+    }
+    override public string ToString()
+    {
+        return "Make: " + Make + "Model " + Model + "Price $" + Price;
     }
 }
